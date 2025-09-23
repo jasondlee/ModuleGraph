dbb31a5e98ba9f7d2bc5d23e1855ab4b4408d85b
Jason Lee <jason@steeplesoft.com>
2025-09-23 14:25:52 -0500

    JavaFX update

--- ModuleGraphApplication.java
+++ ModuleGraphApplication.java
@@ -1,7 +1,7 @@
 /// usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 21+
-//DEPS org.openjfx:javafx-controls:21.0.4
-//DEPS org.openjfx:javafx-fxml:21.0.4
+//DEPS org.openjfx:javafx-controls:23.0.1
+//DEPS org.openjfx:javafx-fxml:23.0.1
 //DEPS com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.18.3
 //DEPS com.google.guava:guava:33.4.8-jre
 //FILES resources/icon.png resources/window.fxml
